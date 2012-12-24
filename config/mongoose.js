8be031db1afcbec8955df6bcca7467bcef8666b6
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-12-24 13:45:21 +0400

    Use config/database.json

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 exports.init = function (compound) {
+    var conf = require('./database.json')[compound.app.set('env')];
     var mongoose = require('mongoose');
-    mongoose.connect('localhost', 'test');
+    mongoose.connect(conf.url);
     require(compound.root + '/db/schema')(mongoose, compound);
 };
